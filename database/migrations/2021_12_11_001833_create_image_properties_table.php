d62c87a1dd52586563d11ad8470830c5fcaf73f6
Kelj-menc <keljmenc@gmail.com>
2021-12-11 03:27:01 +0100

    v15 svasta tinker faker

--- database/migrations/2021_12_11_001833_create_image_properties_table.php
+++ database/migrations/2021_12_11_001833_create_image_properties_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+class CreateImagePropertiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('image_properties', function (Blueprint $table) {
             $table->id();
-            $table->text('body');
-            $table->string('image')->nullable();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('image_properties_id');
+            $table->string('question');
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('image_properties');
     }
 }
